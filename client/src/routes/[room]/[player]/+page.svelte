c96e0ca7ef39cb0ec219fc046fa5bbc164b2772d
loku74 <loicbn7@gmail.com>
2025-12-13 22:47:28 +0100

    better landing in rt room

--- client/src/routes/[room]/[player]/+page.svelte
+++ client/src/routes/[room]/[player]/+page.svelte
@@ -8,25 +8,34 @@
 
   // types
   import type { SocketJoinRoomError, SocketJoinRoomResponse } from "$lib/types/socket";
+  import { USERNAME_MAX_LENGTH } from "$lib/constants";
 
   const room = page.params.room;
   const username = page.params.player;
 
-  let error = $state<string | undefined>(undefined);
+  let roomError = $state<string | undefined>(undefined);
+  let userError = $state<string | undefined>(undefined);
+  let unusualError = $state<string | undefined>(undefined);
   let joined = $state(false);
 
   const socket = getSocket();
 
+  let errors = $derived([roomError, userError, unusualError].filter((e) => e));
+
   function joinRoom() {
     if (joined) return;
 
-    localStorage.setItem("username", username!);
+    localStorage.setItem("username", username?.substring(0, USERNAME_MAX_LENGTH)!);
     socket.emit(
       "join room",
       { username, room },
       (err: SocketJoinRoomError, response: SocketJoinRoomResponse) => {
         if (err) {
-          error = err.username ?? err.room ?? "Failed to join room";
+          roomError = err.room;
+          userError = err.username;
+          if (!userError && !roomError) {
+            unusualError = "Failed to join room";
+          }
           setTimeout(() => {
             goto("/");
           }, 3000);
@@ -49,15 +58,25 @@
   });
 </script>
 
-<div class="flex h-screen items-center justify-center bg-dark-primary">
-  <div class="bg-dark-secondary px-8 py-4 ring-border ring">
-    {#if error}
+<div class="flex h-screen items-center justify-center bg-dark-primary gap-32">
+  {#if !joined}
+    <div class="bg-dark-secondary px-8 py-4 ring-border ring">
       <div class="text-center">
-        <p class="text-red-400 text-xl mb-2">{error}</p>
-        <p class="text-white/50">Redirecting to home...</p>
+        {#each errors as error}
+          {#if error}
+            <p class="text-red-400 text-xl mb-2">{error}</p>
+          {/if}
+        {:else}
+          <p class="text-white text-xl">Joining room "{room}" as "{username}"...</p>
+        {/each}
       </div>
-    {:else if !joined}
-      <p class="text-white text-xl">Joining room "{room}" as "{username}"...</p>
-    {/if}
-  </div>
+    </div>
+  {:else}
+    <div
+      class="p-4 text-center bg-dark-secondary ring ring-inset ring-border h-[640px] w-[320px] flex flex-col"
+    >
+      <h1 class="text-red-primary text-2xl text-ellipsis overflow-hidden">{room}</h1>
+    </div>
+    <div class="bg-dark-secondary ring ring-inset ring-border flex h-[640px] w-[320px]"></div>
+  {/if}
 </div>
