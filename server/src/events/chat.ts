16b87d21f70609a65bfe56d1df17e69448f9f2f1
loku74 <loicbn7@gmail.com>
2026-01-05 14:59:08 +0100

    chat

--- server/src/events/chat.ts
+++ server/src/events/chat.ts
@@ -1,21 +1,21 @@
 import { type Socket } from "socket.io";
-import { users } from "../objects/User";
 import { validateChat } from "../validate/chat";
 import type { SocketChatData } from "client-types";
 import type { Callback } from "../types/types";
 
 export function registerHandlers(socket: Socket) {
   socket.on("chat", (data: SocketChatData, callback: Callback) => {
-    const current = users.get(socket.id);
-    const errors = validateChat(data, current);
+    const result = validateChat(socket, data);
 
-    if (errors) {
-      callback(false, errors);
+    if (!result.status) {
+      callback(false, result.error);
       return;
     }
 
-    current?.socket.to(data.room).emit("message", { from: current.name, message: data.message });
-    console.log(`user ${current?.name} wrote: "${data.message}" to ${data.room} `);
+    result.current.socket
+      .to(result.room)
+      .emit("message", { from: result.current.name, message: result.message });
+    console.log(`user ${result.current.name} wrote: "${result.message}" to ${result.room} `);
     callback(true);
   });
 }
--- server/src/validate/chat.ts
+++ server/src/validate/chat.ts
@@ -1,30 +1,48 @@
+// global
 import z from "zod";
-import { formatSchemeError, messageValidation, roomValidation } from "./validation";
-import type { User } from "../objects/User";
-import { rooms } from "../objects/Room";
-import type { SocketChatData } from "client-types";
 
-const chatSchema = z.object({
+// intern
+import { formatSchemeError, messageValidation, roomValidation } from "./validation";
+import { Room, rooms } from "../objects/Room";
+import { users } from "../objects/User";
+
+// types
+import type { User } from "../objects/User";
+import type { SocketChatData } from "client-types";
+import type { ValidateError } from "../types/server";
+import type { Socket } from "socket.io";
+
+const schema = z.object({
   message: messageValidation,
   room: roomValidation
 });
 
-export function validateChat(
-  data: SocketChatData,
-  current: User | undefined
-): Record<string, string> | null {
-  const result = chatSchema.safeParse(data);
+type ValidateChatSuccess = {
+  status: true;
+  current: User;
+  message: string;
+  room: Room;
+};
+
+type ValidateChatResult = ValidateChatSuccess | ValidateError;
+
+export function validateChat(socket: Socket, data: SocketChatData): ValidateChatResult {
+  const result = schema.safeParse(data);
 
   if (!result.success) {
-    return formatSchemeError(result.error);
+    return { status: false, error: formatSchemeError(result.error) };
   }
+
+  const current = users.get(socket.id);
   const room = rooms.get(data.room);
 
   if (current === undefined) {
-    return { chat: "You do not belong to a room!" };
+    return { status: false, error: { chat: "You do not belong to a room!" } };
   }
+
   if (!current.room || room !== current.room) {
-    return { chat: `You are not in the room ${room?.name}` };
+    return { status: false, error: { chat: `You are not in the room ${room?.name}` } };
   }
-  return null;
+
+  return { status: true, current, message: result.data.message, room };
 }
