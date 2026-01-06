61e6d421de4a1a1c279e544b8e5b8fa94428ec93
ethaaalpha <revenus-nager-0s@icloud.com>
2026-01-06 21:25:52 +0100

    pas besoin de crier

--- server/src/constants/error.ts
+++ server/src/constants/error.ts
@@ -1,20 +1,20 @@
 // commons
-export const NOT_IN_A_ROOM = "You do not belong to a room!";
-export const NOT_IN_THIS_ROOM = "You do not belong to this room!";
-export const PLAYING_ROOM = "This room is already playing!";
-export const INEXISTING_ROOM = "This room does not exist!";
-export const NOT_HOST = "You are not the host of this room!";
+export const NOT_IN_A_ROOM = "You do not belong to a room";
+export const NOT_IN_THIS_ROOM = "You do not belong to this room";
+export const PLAYING_ROOM = "This room is already playing";
+export const INEXISTING_ROOM = "This room does not exist";
+export const NOT_HOST = "You are not the host of this room";
 
 // join room
 export const ROOM_IS_FULL = "Room is full!";
-export const MAX_ROOMS = "Maximum number of rooms reached, please join an existing one!";
-export const USERNAME_TAKEN = "This username is already taken in this room!";
-export const ALREADY_IN_A_ROOM = "You are already in a room!";
+export const MAX_ROOMS = "Maximum number of rooms reached, please join an existing one";
+export const USERNAME_TAKEN = "This username is already taken in this room";
+export const ALREADY_IN_A_ROOM = "You are already in a room";
 
 // kick
-export const KICK_ITSELF = "You can't kick yourself!";
-export const KICK_PLAYING = "You can't kick while playing!";
-export const KICK_INEXISTING = "This user is not in this room!";
+export const KICK_ITSELF = "You can't kick yourself";
+export const KICK_PLAYING = "You can't kick while playing";
+export const KICK_INEXISTING = "This user is not in this room";
 
 // leave room
-export const USER_NOT_FOUND = "User not found!";
+export const USER_NOT_FOUND = "User not found";
