bcbae17fe08b19b8dd3f69e3536c66cd67c6db9e
ethaaalpha <revenus-nager-0s@icloud.com>
2026-01-16 11:35:38 +0100

    add base for movenents

---
+++ server/src/core/movements.ts
@@ -0,0 +1,28 @@
+import type { Player } from "../objects/Player";
+import type { ActionData } from "../types/server";
+
+export const actions = {
+  Rotate: (data: ActionData) => data.piece.rotate90(),
+  Left: (data: ActionData) => data.piece.moveLeft(),
+  Right: (data: ActionData) => data.piece.moveRight(),
+  Down: (data: ActionData) => data.piece.moveDown(),
+  DropDown: (data: ActionData) => {
+    while (data.board.isValidPiece(data.piece.clone().moveDown())) {
+      data.piece.moveDown();
+    }
+  }
+};
+
+export function applyMovement(player: Player, key: keyof typeof actions): boolean {
+  if (player.end) return false;
+
+  const next = player.actualPiece.clone();
+
+  actions[key]({ piece: next, board: player.board } as ActionData);
+
+  if (player.board.isValidPiece(next)) {
+    player.actualPiece = next;
+    return true;
+  }
+  return false;
+}
--- server/src/types/server.ts
+++ server/src/types/server.ts
@@ -1,9 +1,16 @@
-import type { Express } from "express";
 import { Server as HttpServer } from "node:http";
+import type { Express } from "express";
+import type { Board } from "../objects/Board";
+import type { Piece } from "../objects/Piece";
 import type { AppServer } from "./socket";
 
 export interface ServerData {
   app: Express;
   server: HttpServer;
   io: AppServer;
+}
+
+export interface ActionData {
+  piece: Piece;
+  board: Board;
 }
