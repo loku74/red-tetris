5ebd30635d785baaceba6a227d18f976ff831386
ethaaalpha <revenus-nager-0s@icloud.com>
2026-01-16 11:17:05 +0100

    add `Board` class and related IT

---
+++ server/src/objects/Board.ts
@@ -0,0 +1,106 @@
+import { BOARD_HEIGHT, BOARD_WIDTH } from "../constants/core";
+import type { Matrix2D, NonEmptyArray } from "../types/types";
+import { Piece } from "./Piece";
+
+export class Board {
+  public matrix: Matrix2D<number>; // row, column
+  public restrictedLines: number = 0;
+  public placedPieces: number = 0;
+
+  constructor() {
+    // fill grid with 0;
+    this.matrix = Array.from({ length: BOARD_HEIGHT }, () =>
+      Array.from({ length: BOARD_WIDTH }, () => 0)
+    ) as Matrix2D<number>;
+  }
+
+  private getRow(index: number): NonEmptyArray<number> {
+    const result = this.matrix[index];
+    if (!result) throw new Error("Invalid row index!");
+    return result;
+  }
+
+  public isValidPiece(piece: Piece): boolean {
+    // this check if a piece is does not have conflict
+    // with others pieces / walls / restricted lines
+    // useful to detect if a movement is valid or if a piece reach the bottom
+    if (!piece.matrix[0]) throw new Error("Invalid piece matrix!");
+
+    const pieceHeight = piece.matrix.length;
+    const pieceWidth = piece.matrix[0].length;
+
+    for (let i = 0; i < pieceHeight; i++) {
+      for (let j = 0; j < pieceWidth; j++) {
+        const pieceRow = piece.matrix[i];
+        if (!pieceRow) return false;
+        const pieceCell = pieceRow[j];
+        if (pieceCell === undefined) return false;
+
+        if (pieceCell === 1) {
+          const boardRow = this.matrix[piece.x + i];
+          // outside of board
+          if (!boardRow) return false;
+          const boardCell = boardRow[piece.y + j];
+          // outside of board or colisision
+          if (boardCell === undefined || boardCell === 1) return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public place(piece: Piece) {
+    if (!this.isValidPiece(piece)) {
+      throw new Error("Invalid placement");
+    }
+
+    piece.matrix.forEach((pieceRow, i) => {
+      // we skip empty piece lines
+      if (!pieceRow.find((v) => v === 1)) return;
+      const boardRow = this.getRow(piece.x + i);
+
+      pieceRow.forEach((cell, j) => {
+        // skip zeros
+        if (!cell) return;
+        boardRow[piece.y + j] = cell;
+      });
+    });
+    this.placedPieces++;
+  }
+
+  public cleanLines(): number {
+    const start = this.matrix.length - this.restrictedLines - 1;
+    let cleared = 0;
+
+    // iterate from bottom to top
+    for (let rowIndex = start; rowIndex >= 0; rowIndex--) {
+      const row = this.getRow(rowIndex);
+
+      const toClear = row.every((v) => v === 1);
+      if (toClear) {
+        cleared++;
+        // push down every lines upper to the line cleared
+        for (let k = rowIndex; k >= 1; k--) {
+          const rowBefore = this.getRow(k - 1);
+          const rowActual = this.getRow(k);
+
+          rowActual.forEach((_, j) => {
+            if (rowBefore[j] === undefined) throw new Error("Invalid column index!");
+            rowActual[j] = rowBefore[j];
+          });
+        }
+        this.matrix[0].fill(0);
+        rowIndex++;
+      }
+    }
+    return cleared;
+  }
+
+  public addRestrictedLine() {
+    const row = this.getRow(this.restrictedLines);
+    row.forEach((_, i) => {
+      row[i] = 1;
+    });
+    this.restrictedLines++;
+  }
+}
---
+++ server/src/tests/board.test.ts
@@ -0,0 +1,127 @@
+import { describe, expect, it, beforeEach } from "vitest";
+import { Board } from "../objects/Board";
+import { Piece } from "../objects/Piece";
+import { PIECES } from "../constants/pieces";
+
+let board: Board;
+
+beforeEach(() => {
+  board = new Board();
+});
+
+describe("invalid placements", () => {
+  it("top right", () => {
+    expect(board.isValidPiece(new Piece("I", PIECES.I.matrix, -1, 7))).toBe(false);
+    expect(board.isValidPiece(new Piece("J", PIECES.J.matrix, 0, 8))).toBe(false);
+    expect(board.isValidPiece(new Piece("L", PIECES.L.matrix, 0, 8))).toBe(false);
+    expect(board.isValidPiece(new Piece("O", PIECES.O.matrix, 0, 9))).toBe(false);
+    expect(board.isValidPiece(new Piece("S", PIECES.S.matrix, -1, 7))).toBe(false);
+    expect(board.isValidPiece(new Piece("T", PIECES.T.matrix, -1, 8))).toBe(false);
+    expect(board.isValidPiece(new Piece("Z", PIECES.Z.matrix, 0, 8))).toBe(false);
+  });
+
+  it("top left", () => {
+    expect(board.isValidPiece(new Piece("I", PIECES.I.matrix, -1, -1))).toBe(false);
+    expect(board.isValidPiece(new Piece("J", PIECES.J.matrix, 0, -1))).toBe(false);
+    expect(board.isValidPiece(new Piece("L", PIECES.L.matrix, -1, 0))).toBe(false);
+    expect(board.isValidPiece(new Piece("O", PIECES.O.matrix, 0, -2))).toBe(false);
+    expect(board.isValidPiece(new Piece("S", PIECES.S.matrix, 0, -1))).toBe(false);
+    expect(board.isValidPiece(new Piece("T", PIECES.T.matrix, 0, -1))).toBe(false);
+    expect(board.isValidPiece(new Piece("Z", PIECES.Z.matrix, 0, -1))).toBe(false);
+  });
+
+  it("bottom right", () => {
+    expect(board.isValidPiece(new Piece("I", PIECES.I.matrix, 18, 7))).toBe(false);
+    expect(board.isValidPiece(new Piece("J", PIECES.J.matrix, 17, 8))).toBe(false);
+    expect(board.isValidPiece(new Piece("L", PIECES.L.matrix, 17, 8))).toBe(false);
+    expect(board.isValidPiece(new Piece("O", PIECES.O.matrix, 17, 9))).toBe(false);
+    expect(board.isValidPiece(new Piece("S", PIECES.S.matrix, 19, 7))).toBe(false);
+    expect(board.isValidPiece(new Piece("T", PIECES.T.matrix, 17, 8))).toBe(false);
+    expect(board.isValidPiece(new Piece("Z", PIECES.Z.matrix, 17, 8))).toBe(false);
+  });
+
+  it("bottom left", () => {
+    expect(board.isValidPiece(new Piece("I", PIECES.I.matrix, 17, -1))).toBe(false);
+    expect(board.isValidPiece(new Piece("J", PIECES.J.matrix, 17, -1))).toBe(false);
+    expect(board.isValidPiece(new Piece("L", PIECES.L.matrix, 19, 0))).toBe(false);
+    expect(board.isValidPiece(new Piece("O", PIECES.O.matrix, 17, -2))).toBe(false);
+    expect(board.isValidPiece(new Piece("S", PIECES.S.matrix, 17, -1))).toBe(false);
+    expect(board.isValidPiece(new Piece("T", PIECES.T.matrix, 17, -1))).toBe(false);
+    expect(board.isValidPiece(new Piece("Z", PIECES.Z.matrix, 17, -1))).toBe(false);
+  });
+});
+
+describe("valid placements", () => {
+  it("top right", () => {
+    expect(board.isValidPiece(new Piece("I", PIECES.I.matrix, -1, 6))).toBe(true);
+    expect(board.isValidPiece(new Piece("J", PIECES.J.matrix, 0, 7))).toBe(true);
+    expect(board.isValidPiece(new Piece("L", PIECES.L.matrix, 0, 7))).toBe(true);
+    expect(board.isValidPiece(new Piece("O", PIECES.O.matrix, 0, 7))).toBe(true);
+    expect(board.isValidPiece(new Piece("S", PIECES.S.matrix, 0, 7))).toBe(true);
+    expect(board.isValidPiece(new Piece("T", PIECES.T.matrix, 0, 7))).toBe(true);
+    expect(board.isValidPiece(new Piece("Z", PIECES.Z.matrix, 0, 7))).toBe(true);
+  });
+
+  it("top left", () => {
+    expect(board.isValidPiece(new Piece("I", PIECES.I.matrix, -1, 0))).toBe(true);
+    expect(board.isValidPiece(new Piece("J", PIECES.J.matrix, 0, 0))).toBe(true);
+    expect(board.isValidPiece(new Piece("L", PIECES.L.matrix, 0, 0))).toBe(true);
+    expect(board.isValidPiece(new Piece("O", PIECES.O.matrix, 0, -1))).toBe(true);
+    expect(board.isValidPiece(new Piece("S", PIECES.S.matrix, 0, 0))).toBe(true);
+    expect(board.isValidPiece(new Piece("T", PIECES.T.matrix, 0, 0))).toBe(true);
+    expect(board.isValidPiece(new Piece("Z", PIECES.Z.matrix, 0, 0))).toBe(true);
+  });
+
+  it("bottom right", () => {
+    expect(board.isValidPiece(new Piece("I", PIECES.I.matrix, 18, 6))).toBe(true);
+    expect(board.isValidPiece(new Piece("J", PIECES.J.matrix, 18, 7))).toBe(true);
+    expect(board.isValidPiece(new Piece("L", PIECES.L.matrix, 18, 7))).toBe(true);
+    expect(board.isValidPiece(new Piece("O", PIECES.O.matrix, 18, 7))).toBe(true);
+    expect(board.isValidPiece(new Piece("S", PIECES.S.matrix, 18, 7))).toBe(true);
+    expect(board.isValidPiece(new Piece("T", PIECES.T.matrix, 18, 7))).toBe(true);
+    expect(board.isValidPiece(new Piece("Z", PIECES.Z.matrix, 18, 7))).toBe(true);
+  });
+
+  it("bottom left", () => {
+    expect(board.isValidPiece(new Piece("I", PIECES.I.matrix, 18, 0))).toBe(true);
+    expect(board.isValidPiece(new Piece("J", PIECES.J.matrix, 18, 0))).toBe(true);
+    expect(board.isValidPiece(new Piece("L", PIECES.L.matrix, 18, 0))).toBe(true);
+    expect(board.isValidPiece(new Piece("O", PIECES.O.matrix, 18, -1))).toBe(true);
+    expect(board.isValidPiece(new Piece("S", PIECES.S.matrix, 18, 0))).toBe(true);
+    expect(board.isValidPiece(new Piece("T", PIECES.T.matrix, 18, 0))).toBe(true);
+    expect(board.isValidPiece(new Piece("Z", PIECES.Z.matrix, 18, 0))).toBe(true);
+  });
+});
+
+it("valid placement", () => {
+  const piece = new Piece("T", PIECES.T.matrix, 18, 4);
+
+  expect(board.isValidPiece(piece)).toBe(true);
+  board.place(piece);
+
+  expect(board.matrix[18]?.slice(4, 7)).toEqual([0, 1, 0]);
+  expect(board.matrix[19]?.slice(4, 7)).toEqual([1, 1, 1]);
+});
+
+it("clear lines", () => {
+  const board = new Board();
+
+  // first incomplete line
+  board.place(new Piece("I", PIECES.I.matrix, -1, 0));
+  board.place(new Piece("S", PIECES.S.matrix, 0, 7));
+
+  // two others random lines (filled)
+  board.place(new Piece("I", PIECES.I.matrix, 5, 0));
+  board.place(new Piece("I", PIECES.I.matrix, 5, 4));
+  board.place(new Piece("S", PIECES.S.matrix, 6, 7));
+  board.place(new Piece("I", PIECES.I.matrix, 10, 0));
+  board.place(new Piece("I", PIECES.I.matrix, 10, 4));
+  board.place(new Piece("S", PIECES.S.matrix, 11, 7));
+
+  expect(board.cleanLines()).toEqual(2);
+
+  // check if the first line have dropped twice
+  expect(board.matrix[2]?.slice(0, 4)).toEqual([1, 1, 1, 1]);
+  expect(board.matrix[2]?.slice(8, 10)).toEqual([1, 1]);
+  expect(board.matrix[3]?.slice(7, 9)).toEqual([1, 1]);
+});
