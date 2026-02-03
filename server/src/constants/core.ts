6448d9fcb0c08e49dfa6df689d8ccbea3d60e687
Ethaniel <30417812+ethaaalpha@users.noreply.github.com>
2026-02-03 10:46:29 +0100

    feature: Board class and Piece placement (#71)
    * improve `Piece` class
    
    - added Color, coordinate
    - added movement modifiers
    
    * add `asMatrix` helper method
    
    * improve `Piece` class
    
    - added Color, coordinate
    - added movement modifiers
    
    * add `Board` class and related IT
    
    * update imports
    
    * simplify `isValidPiece`
    
    * add board as const

--- server/src/constants/core.ts
+++ server/src/constants/core.ts
@@ -1,10 +1,18 @@
+// system
 export const USERNAME_MAX_LENGTH = Number(process.env.USERNAME_MAX_LENGTH) || 16;
+
+// room
 export const ROOM_MAX_LENGTH = Number(process.env.ROOM_MAX_LENGTH) || 16;
 export const ROOM_MAX_USERS = Number(process.env.ROOM_MAX_USERS) || 8;
 export const ROOM_MAX = Number(process.env.ROOM_MAX) || 1024;
+
+// chat & user
 export const SERVER_PORT = Number(process.env.SERVER_PORT) || 8080;
 export const CHAT_MAX_LENGTH = Number(process.env.CHAT_MAX_LENGTH) || 128;
+
+// game
 export const WARMUP_RESTART_DELAY = Number(process.env.RESTART_DELAY) || 5;
 
+// regex
 export const REGEX_ROOM_AND_USER = /^[a-zA-Z0-9_-]+$/;
 export const REGEX_MESSAGE = /^[\P{C}]+$/u;
