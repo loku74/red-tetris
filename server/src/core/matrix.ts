82e2ed0aa48249fa20e022172e2a75620fc93e73
ethaaalpha <revenus-nager-0s@icloud.com>
2026-02-17 17:37:20 +0100

    cleaning

--- server/src/core/matrix.ts
+++ server/src/core/matrix.ts
@@ -1,23 +1,6 @@
-import type { Matrix2D, NonEmptyArray } from "@app/shared";
+import type { Matrix2D } from "@app/shared";
 
 import type { Piece } from "@app/objects/Piece";
-
-export function asMatrix(data: (number | undefined)[][]): Matrix2D<number> {
-  const rows: NonEmptyArray<number>[] = data.map((row) => {
-    const checked: number[] = row.map((cell) => {
-      if (cell === undefined) throw new Error("Cell cannot be undefined!");
-      return cell;
-    });
-    const [first, ...others] = checked;
-    if (first === undefined) throw new Error("Cell cannot be undefined!");
-
-    return [first, ...others];
-  });
-  const [first, ...others] = rows;
-  if (first === undefined) throw new Error("Row cannot be undefined!");
-
-  return [first, ...others];
-}
 
 export function placePieceOnMatrix(piece: Piece, matrix: Matrix2D<number>) {
   piece.blocks.forEach(([x, y]) => {
