de3d52d99f506e18c2ba6fcbf349c7d08906edca
loku74 <loicbn7@gmail.com>
2026-03-02 21:50:20 +0100

    checkbox for destructible penality

---
+++ client/src/lib/components/Checkbox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Check } from "@lucide/svelte";
+
+  let { checked = $bindable(false), id }: { checked: boolean; id: string } = $props();
+</script>
+
+<button
+  onclick={() => (checked = !checked)}
+  {id}
+  class="w-5 h-5 rounded border duration-75 grid
+  {checked ? 'border-red-accent/64' : 'border-border/64 hover:border-border'}"
+>
+  {#if checked}
+    <Check class="text-red-accent w-4 h-4 place-self-center pt-0.5" />
+  {/if}
+</button>
