56d4924df688888665f104a3ab10b6b4ca299964
ethaaalpha <revenus-nager-0s@icloud.com>
2026-03-02 21:10:12 +0100

    missing file

--- shared/constants/limits.ts
+++ shared/constants/limits.ts
@@ -5,9 +5,11 @@ export const ROOM_MAX_LENGTH = 16;
 export const MESSAGE_MAX_LENGTH = 128;
 export const GAME_MIN_PLAYERS = 2;
 export const GAME_TICK_MIN = 100;
+export const GAME_TICK_DEFAULT = 500;
 export const GAME_TICK_MAX = 1000;
+export const GAME_DESTRUCTIBLE_DEFAULT = false;
 
 export const DEFAULT_GAME_SETTINGS: GameSettings = {
-  tick: 500,
-  destructiblePenality: false
+  tick: GAME_TICK_DEFAULT,
+  destructiblePenality: GAME_DESTRUCTIBLE_DEFAULT
 }
