21678976c8a7eb8aca02f75f7825fad153e1e909
loku74 <loicbn7@gmail.com>
2026-03-05 10:50:30 +0100

    rc-1

--- client/src/lib/components/Game/ScorePopup.svelte
+++ client/src/lib/components/Game/ScorePopup.svelte
@@ -1,41 +1,43 @@
 <script lang="ts">
-  import type { GameScore } from "@app/shared";
+  import { fade } from "svelte/transition";
 
-  let { gameScore }: { gameScore: GameScore } = $props();
+  import type { GameScore, UserData } from "@app/shared";
+
+  let {
+    gameScore,
+    dead,
+    spectatedPlayer
+  }: { gameScore?: GameScore; dead: boolean; spectatedPlayer?: UserData } = $props();
 </script>
 
 {#key gameScore}
-  <div class="absolute inset-0 flex items-center justify-center pointer-events-none">
-    <div class="flex flex-col items-center gap-1 animate-score-popup">
-      <span class="text-red-accent text-sm uppercase tracking-widest">
-        {gameScore.type}
-      </span>
-      <span class="text-red-accent text-2xl">
-        +{gameScore.score}
-      </span>
+  {#if gameScore}
+    <div class="absolute inset-0 flex items-center justify-center pointer-events-none">
+      <div class="flex flex-col items-center gap-1 animate-score-popup">
+        <span class="text-red-accent text-sm uppercase tracking-widest">
+          {gameScore.type}
+        </span>
+        <span class="text-red-accent text-2xl">
+          +{gameScore.score}
+        </span>
+      </div>
     </div>
-  </div>
+  {/if}
 {/key}
 
-<style>
-  @keyframes score-popup {
-    0% {
-      opacity: 0;
-      transform: translateY(0);
-    }
-    20% {
-      opacity: 1;
-    }
-    80% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 0;
-      transform: translateY(-42px);
-    }
-  }
+{#if dead}
+  <div
+    in:fade={{ duration: 442 }}
+    class="absolute inset-0 flex items-center justify-center pointer-events-none animate-dead-popup {spectatedPlayer
+      ? 'opacity-0'
+      : ''}"
+  >
+    <span class="text-red-accent text-4xl uppercase tracking-widest">you lose!</span>
+  </div>
+{/if}
 
+<style>
   .animate-score-popup {
-    animation: score-popup 1.2s ease-out forwards;
+    animation: test-popup 1.2s ease-out forwards;
   }
 </style>
